e8fa7911fed017ce44f511302cf2ddb493705390
maybe-joe <josephherk@gmail.com>
2025-12-14 19:27:02 +1300

    Implement parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,9 @@
 package parser
 
 import (
+	"strconv"
+
+	"github.com/maybe-joe/monkey/ast"
 	"github.com/maybe-joe/monkey/token"
 )
 
@@ -8,6 +11,37 @@ var (
 	ErrExpectedIdentifier = "expected identifier after let"
 	ErrExpectedAssignment = "expected assignment after identifier"
 	ErrExpectedSemicolon  = "expected semicolon after expression"
+)
+
+// Order of precedence
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+)
+
+// Precedences maps token types to their precedence level
+// for infix operators.
+var precedences = map[token.TokenType]int{
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+	token.EQ:       EQUALS,
+	token.NOT_EQ:   EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.LPAREN:   CALL,
+}
+
+type (
+	prefixFn func() ast.Expression
+	infixFn  func(ast.Expression) ast.Expression
 )
 
 type Tokenizer interface {
@@ -19,6 +53,9 @@ type Parser struct {
 	current   token.Token
 	next      token.Token
 	errors    []string
+
+	prefixLookup map[token.TokenType]prefixFn
+	infixLookup  map[token.TokenType]infixFn
 }
 
 func New(tokenizer Tokenizer) *Parser {
@@ -27,16 +64,39 @@ func New(tokenizer Tokenizer) *Parser {
 		errors:    []string{},
 	}
 
+	p.prefixLookup = map[token.TokenType]prefixFn{
+		token.IDENT:    p.Identifier,
+		token.INT:      p.Integer,
+		token.BANG:     p.Prefix,
+		token.MINUS:    p.Prefix,
+		token.TRUE:     p.Boolean,
+		token.FALSE:    p.Boolean,
+		token.LPAREN:   p.Group,
+		token.IF:       p.If,
+		token.FUNCTION: p.Function,
+	}
+
+	p.infixLookup = map[token.TokenType]infixFn{
+		token.PLUS:     p.Infix,
+		token.MINUS:    p.Infix,
+		token.SLASH:    p.Infix,
+		token.ASTERISK: p.Infix,
+		token.EQ:       p.Infix,
+		token.NOT_EQ:   p.Infix,
+		token.LT:       p.Infix,
+		token.GT:       p.Infix,
+		token.LPAREN:   p.Call,
+	}
+
 	p.Next()
 	p.Next()
 
 	return p
 }
 
-func (p *Parser) Let() *Let {
+func (p *Parser) Let() *ast.Let {
 	// If the next token is not an identifier the code is invalid.
 	if !p.next.Is(token.IDENT) {
-		p.errors = append(p.errors, ErrExpectedIdentifier)
 		return nil
 	}
 
@@ -44,63 +104,299 @@ func (p *Parser) Let() *Let {
 	p.Next()
 
 	// Create the identifier node.
-	id := Identifier{
+	id := ast.Identifier{
 		Value: p.current.Literal,
 	}
 
 	// Next we expect an assignment token.
 	if !p.next.Is(token.ASSIGN) {
-		p.errors = append(p.errors, ErrExpectedAssignment)
 		return nil
 	}
 
 	// Advance to the assignment token.
 	p.Next()
+	p.Next()
 
-	// For now we will skip the expression until we reach a semicolon.
-	for !(p.next.Is(token.SEMICOLON) || p.next.Is(token.EOF)) {
+	// The next token should be the start of the expression.
+	expr := p.Expression(LOWEST)
+
+	// Possibly advance to the semicolon.
+	if p.next.Is(token.SEMICOLON) {
 		p.Next()
 	}
 
-	// Advance to the semicolon.
-	p.Next()
-
-	if !p.current.Is(token.SEMICOLON) {
-		p.errors = append(p.errors, ErrExpectedSemicolon)
-		return nil
-	}
-
-	return &Let{
+	return &ast.Let{
 		Identifier: &id,
+		Value:      expr,
 	}
 }
 
-func (p *Parser) Return() *Return {
+func (p *Parser) Return() *ast.Return {
 	// Advance to the expression token.
 	p.Next()
 
-	// For now we will skip the expression until we reach a semicolon.
-	for !(p.next.Is(token.SEMICOLON) || p.next.Is(token.EOF)) {
+	// The next token should be the start of the expression.
+	expr := p.Expression(LOWEST)
+
+	// Possibly advance to the semicolon.
+	if p.next.Is(token.SEMICOLON) {
 		p.Next()
 	}
 
-	// Advance to the semicolon.
-	p.Next()
-
-	if !p.current.Is(token.SEMICOLON) {
-		p.errors = append(p.errors, ErrExpectedSemicolon)
-		return nil
-	}
-
-	return &Return{
-		// Value: &IntegerLiteral{Value: 5},
+	return &ast.Return{
+		Value: expr,
 	}
 }
 
-func (p *Parser) Statement() Statement {
+func (p *Parser) If() ast.Expression {
+	if !p.next.Is(token.LPAREN) {
+		return nil
+	}
+
+	p.Next()
+	p.Next()
+
+	condition := p.Expression(LOWEST)
+
+	if !p.next.Is(token.RPAREN) {
+		return nil
+	}
+
+	p.Next()
+
+	if !p.next.Is(token.LBRACE) {
+		return nil
+	}
+
+	p.Next()
+
+	consequence := p.Block()
+
+	var alternative *ast.Block
+	if p.next.Is(token.ELSE) {
+		p.Next()
+
+		if !p.next.Is(token.LBRACE) {
+			return nil
+		}
+
+		p.Next()
+
+		alternative = p.Block()
+	}
+
+	return &ast.If{
+		Condition:   condition,
+		Consequence: consequence,
+		Alternative: alternative,
+	}
+}
+
+func (p *Parser) Block() *ast.Block {
+	stmts := []ast.Statement{}
+
+	p.Next()
+
+	for !p.current.Is(token.RBRACE) && !p.current.Is(token.EOF) {
+		if stmt := p.Statement(); stmt != nil {
+			stmts = append(stmts, stmt)
+		}
+		p.Next()
+	}
+
+	return &ast.Block{
+		Statements: stmts,
+	}
+}
+
+func (p *Parser) Identifier() ast.Expression {
+	return &ast.Identifier{
+		Value: p.current.Literal,
+	}
+}
+
+func (p *Parser) Integer() ast.Expression {
+	i, err := strconv.ParseInt(p.current.Literal, 0, 64)
+	if err != nil {
+		p.errors = append(p.errors, "could not parse "+p.current.Literal+" as integer")
+		return nil
+	}
+
+	return &ast.Integer{
+		Value: i,
+	}
+}
+
+func (p *Parser) Boolean() ast.Expression {
+	return &ast.Boolean{
+		Value: p.current.Is(token.TRUE),
+	}
+}
+
+func (p *Parser) Call(function ast.Expression) ast.Expression {
+	return &ast.Call{
+		Function:  function,
+		Arguments: p.Arguments(),
+	}
+}
+
+func (p *Parser) Arguments() []ast.Expression {
+	if p.next.Is(token.RPAREN) {
+		p.Next()
+		return nil
+	}
+
+	p.Next()
+
+	args := []ast.Expression{
+		p.Expression(LOWEST),
+	}
+
+	for p.next.Is(token.COMMA) {
+		p.Next()
+		p.Next()
+		args = append(args, p.Expression(LOWEST))
+	}
+
+	if !p.next.Is(token.RPAREN) {
+		return nil
+	}
+
+	p.Next()
+	return args
+}
+
+func (p *Parser) Function() ast.Expression {
+	if !p.next.Is(token.LPAREN) {
+		return nil
+	}
+
+	p.Next()
+
+	parameters := p.Parameters()
+
+	if !p.next.Is(token.LBRACE) {
+		return nil
+	}
+
+	p.Next()
+
+	body := p.Block()
+
+	return &ast.Function{
+		Parameters: parameters,
+		Body:       body,
+	}
+}
+
+func (p *Parser) Parameters() []*ast.Identifier {
+	if p.next.Is(token.RPAREN) {
+		p.Next()
+		return nil
+	}
+
+	p.Next()
+
+	identifiers := []*ast.Identifier{
+		{Value: p.current.Literal},
+	}
+
+	for p.next.Is(token.COMMA) {
+		p.Next()
+		p.Next()
+		identifiers = append(identifiers, &ast.Identifier{Value: p.current.Literal})
+	}
+
+	if !p.next.Is(token.RPAREN) {
+		return nil
+	}
+
+	p.Next()
+
+	return identifiers
+}
+
+func (p *Parser) Group() ast.Expression {
+	p.Next()
+
+	expr := p.Expression(LOWEST)
+
+	if !p.next.Is(token.RPAREN) {
+		return nil
+	}
+
+	p.Next()
+
+	return expr
+}
+
+func (p *Parser) Prefix() ast.Expression {
+	expr := &ast.Prefix{
+		Operator: p.current.String(),
+	}
+
+	p.Next()
+
+	expr.Right = p.Expression(PREFIX)
+
+	return expr
+}
+
+func (p *Parser) Infix(left ast.Expression) ast.Expression {
+	expr := &ast.Infix{
+		Left:     left,
+		Operator: p.current.String(),
+	}
+
+	precedence := LOWEST
+	if val, ok := precedences[p.current.Type]; ok {
+		precedence = val
+	}
+
+	p.Next()
+	expr.Right = p.Expression(precedence)
+
+	return expr
+}
+
+func (p *Parser) Expression(precedence int) ast.Expression {
+	prefix, ok := p.prefixLookup[p.current.Type]
+	if !ok {
+		return nil
+	}
+
+	expr := prefix()
+
+	for !p.next.Is(token.SEMICOLON) && precedence < precedences[p.next.Type] {
+		infix, ok := p.infixLookup[p.next.Type]
+		if !ok {
+			return expr
+		}
+
+		p.Next()
+
+		expr = infix(expr)
+	}
+
+	return expr
+}
+
+func (p *Parser) ExpressionStatement() *ast.ExpressionStatement {
+	expr := p.Expression(LOWEST)
+
+	if p.next.Is(token.SEMICOLON) {
+		p.Next()
+	}
+
+	return &ast.ExpressionStatement{
+		Expression: expr,
+	}
+}
+
+func (p *Parser) Statement() ast.Statement {
 	switch p.current.Type {
 	default:
-		return nil
+		return p.ExpressionStatement()
 	case token.LET:
 		return p.Let()
 	case token.RETURN:
@@ -113,8 +409,14 @@ func (p *Parser) Next() {
 	p.next = p.tokenizer.Next()
 }
 
-func (p *Parser) Parse() *Root {
-	root := &Root{}
+func (p *Parser) Until(typ token.TokenType) {
+	for !p.current.Is(typ) && !p.current.Is(token.EOF) {
+		p.Next()
+	}
+}
+
+func (p *Parser) Parse() *ast.Root {
+	root := &ast.Root{}
 
 	for p.current.Type != token.EOF {
 		if stmt := p.Statement(); stmt != nil {
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -3,9 +3,9 @@ package parser
 import (
 	"testing"
 
+	"github.com/maybe-joe/monkey/ast"
 	"github.com/maybe-joe/monkey/token"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func Test_Program(t *testing.T) {
@@ -15,24 +15,28 @@ func Test_Program(t *testing.T) {
 		let foobar = 838383;
 
 		return 5;
+		foobar;
 	`
 
-	expected := &Root{
-		Statements: []Statement{
-			&Let{
-				Identifier: &Identifier{Value: "x"},
-				// Value:      &IntegerLiteral{Value: 5},
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.Let{
+				Identifier: &ast.Identifier{Value: "x"},
+				Value:      &ast.Integer{Value: 5},
 			},
-			&Let{
-				Identifier: &Identifier{Value: "y"},
-				// Value:      &IntegerLiteral{Value: 10},
+			&ast.Let{
+				Identifier: &ast.Identifier{Value: "y"},
+				Value:      &ast.Integer{Value: 10},
 			},
-			&Let{
-				Identifier: &Identifier{Value: "foobar"},
-				// Value:      &IntegerLiteral{Value: 838383},
+			&ast.Let{
+				Identifier: &ast.Identifier{Value: "foobar"},
+				Value:      &ast.Integer{Value: 838383},
 			},
-			&Return{
-				// Value: &IntegerLiteral{Value: 5},
+			&ast.Return{
+				Value: &ast.Integer{Value: 5},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Identifier{Value: "foobar"},
 			},
 		},
 	}
@@ -42,76 +46,297 @@ func Test_Program(t *testing.T) {
 }
 
 func Test_Let(t *testing.T) {
-	t.Run("happy path", func(t *testing.T) {
-		given := `
-			let x = 5;
-		`
+	given := `
+		let x = 67;
+		let y = x;
+	`
 
-		expected := &Root{
-			Statements: []Statement{
-				&Let{
-					Identifier: &Identifier{Value: "x"},
-					// Value:      &IntegerLiteral{Value: 5},
-				},
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.Let{
+				Identifier: &ast.Identifier{Value: "x"},
+				Value:      &ast.Integer{Value: 67},
 			},
-		}
+			&ast.Let{
+				Identifier: &ast.Identifier{Value: "y"},
+				Value:      &ast.Identifier{Value: "x"},
+			},
+		},
+	}
 
-		actual := New(token.NewTokenizer(given)).Parse()
-		assert.Equal(t, expected, actual)
-	})
-
-	t.Run("expected identifier", func(t *testing.T) {
-		given := `
-			let = 5;
-		`
-
-		p := New(token.NewTokenizer(given))
-		_ = p.Parse()
-
-		require.Len(t, p.errors, 1)
-		assert.Equal(t, ErrExpectedIdentifier, p.errors[0])
-	})
-
-	t.Run("expected assignment", func(t *testing.T) {
-		given := `
-			let x 5;
-		`
-
-		p := New(token.NewTokenizer(given))
-		_ = p.Parse()
-
-		require.Len(t, p.errors, 1)
-		assert.Equal(t, ErrExpectedAssignment, p.errors[0])
-	})
-
-	t.Run("expected semicolon", func(t *testing.T) {
-		given := `
-			let x = 5
-		`
-
-		p := New(token.NewTokenizer(given))
-		_ = p.Parse()
-
-		require.Len(t, p.errors, 1)
-		assert.Equal(t, ErrExpectedSemicolon, p.errors[0])
-	})
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
 }
 
 func Test_Return(t *testing.T) {
-	t.Run("happy path", func(t *testing.T) {
-		given := `
-			return 5;
-		`
+	given := `
+		return 67;
+		return x;
+	`
 
-		expected := &Root{
-			Statements: []Statement{
-				&Return{
-					// Value: &IntegerLiteral{Value: 5},
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.Return{
+				Value: &ast.Integer{Value: 67},
+			},
+			&ast.Return{
+				Value: &ast.Identifier{Value: "x"},
+			},
+		},
+	}
+
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Prefix(t *testing.T) {
+	given := `
+		!5;
+		-15;
+	`
+
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.ExpressionStatement{
+				Expression: &ast.Prefix{
+					Operator: "!",
+					Right:    &ast.Integer{Value: 5},
 				},
 			},
-		}
+			&ast.ExpressionStatement{
+				Expression: &ast.Prefix{
+					Operator: "-",
+					Right:    &ast.Integer{Value: 15},
+				},
+			},
+		},
+	}
 
-		actual := New(token.NewTokenizer(given)).Parse()
-		assert.Equal(t, expected, actual)
-	})
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Infix(t *testing.T) {
+	given := `
+		5 + 5;
+		10 - 2;
+		3 * 4;
+		8 / 2;
+		5 > 3;
+		2 < 4;
+		6 == 6;
+		7 != 9;
+	`
+
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left:     &ast.Integer{Value: 5},
+					Operator: "+",
+					Right:    &ast.Integer{Value: 5},
+				},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left:     &ast.Integer{Value: 10},
+					Operator: "-",
+					Right:    &ast.Integer{Value: 2},
+				},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left:     &ast.Integer{Value: 3},
+					Operator: "*",
+					Right:    &ast.Integer{Value: 4},
+				},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left:     &ast.Integer{Value: 8},
+					Operator: "/",
+					Right:    &ast.Integer{Value: 2},
+				},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left:     &ast.Integer{Value: 5},
+					Operator: ">",
+					Right:    &ast.Integer{Value: 3},
+				},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left:     &ast.Integer{Value: 2},
+					Operator: "<",
+					Right:    &ast.Integer{Value: 4},
+				},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left:     &ast.Integer{Value: 6},
+					Operator: "==",
+					Right:    &ast.Integer{Value: 6},
+				},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left:     &ast.Integer{Value: 7},
+					Operator: "!=",
+					Right:    &ast.Integer{Value: 9},
+				},
+			},
+		},
+	}
+
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Boolean(t *testing.T) {
+	given := `
+		true;
+		false;
+	`
+
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.ExpressionStatement{
+				Expression: &ast.Boolean{Value: true},
+			},
+			&ast.ExpressionStatement{
+				Expression: &ast.Boolean{Value: false},
+			},
+		},
+	}
+
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Group(t *testing.T) {
+	given := `
+		(5 + 5) * 2;
+	`
+
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.ExpressionStatement{
+				Expression: &ast.Infix{
+					Left: &ast.Infix{
+						Left:     &ast.Integer{Value: 5},
+						Operator: "+",
+						Right:    &ast.Integer{Value: 5},
+					},
+					Operator: "*",
+					Right:    &ast.Integer{Value: 2},
+				},
+			},
+		},
+	}
+
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
+}
+
+func Test_If(t *testing.T) {
+	given := `
+		if (x < y) { x } else { y }
+	`
+
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.ExpressionStatement{
+				Expression: &ast.If{
+					Condition: &ast.Infix{
+						Left:     &ast.Identifier{Value: "x"},
+						Operator: "<",
+						Right:    &ast.Identifier{Value: "y"},
+					},
+					Consequence: &ast.Block{
+						Statements: []ast.Statement{
+							&ast.ExpressionStatement{
+								Expression: &ast.Identifier{Value: "x"},
+							},
+						},
+					},
+					Alternative: &ast.Block{
+						Statements: []ast.Statement{
+							&ast.ExpressionStatement{
+								Expression: &ast.Identifier{Value: "y"},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Function(t *testing.T) {
+	given := `
+		fn(x, y) { x + y; }
+	`
+
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.ExpressionStatement{
+				Expression: &ast.Function{
+					Parameters: []*ast.Identifier{
+						{Value: "x"},
+						{Value: "y"},
+					},
+					Body: &ast.Block{
+						Statements: []ast.Statement{
+							&ast.ExpressionStatement{
+								Expression: &ast.Infix{
+									Left:     &ast.Identifier{Value: "x"},
+									Operator: "+",
+									Right:    &ast.Identifier{Value: "y"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
+}
+
+func Test_Call(t *testing.T) {
+	given := `
+		add(1, 2 + 3, 4 * 5);
+	`
+
+	expected := &ast.Root{
+		Statements: []ast.Statement{
+			&ast.ExpressionStatement{
+				Expression: &ast.Call{
+					Function: &ast.Identifier{Value: "add"},
+					Arguments: []ast.Expression{
+						&ast.Integer{Value: 1},
+						&ast.Infix{
+							Left:     &ast.Integer{Value: 2},
+							Operator: "+",
+							Right:    &ast.Integer{Value: 3},
+						},
+						&ast.Infix{
+							Left:     &ast.Integer{Value: 4},
+							Operator: "*",
+							Right:    &ast.Integer{Value: 5},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	actual := New(token.NewTokenizer(given)).Parse()
+	assert.Equal(t, expected, actual)
 }
